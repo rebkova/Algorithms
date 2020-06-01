cb4d48e8cca51b862a021b6383e15bc1c9cb1dde
rebkova <42389113+rebkova@users.noreply.github.com>
2020-06-01 09:50:34 +0200

    Add files via upload

---
+++ Binary_search_program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Binary_search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*binary search function*/
+            int[] primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
+        41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
+            
+            doSearch(primes, 79);
+                        
+        }
+
+        /*method defined*/
+        private static void doSearch(int[] data, int searchItem)
+        {
+            int min = 0;
+            int max = data.Length -1;
+            int guess;
+            int count = 0;
+
+            while (max >= min)
+            {
+                guess = (int)(Math.Floor((max + min) / 2.0));
+                if (data[guess] == searchItem)
+                {
+                    Console.WriteLine("Congrats, the number you're looking for is at index " + guess);
+                    break;
+                }
+                
+                else if (data[guess] > searchItem)
+                {
+                    max = guess - 1;
+                }
+
+                else
+                {
+                    min = guess + 1;
+                }
+                count++;
+            }
+
+            while (max < min)
+            {
+                Console.WriteLine("-1");
+                break;
+            }
+            Console.WriteLine("Count:" + count);
+            
+
+        }
+    }
+}
