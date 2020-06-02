ca5191cd1a48b77bc1781b8db64cfedec3964b3a
rebkova <42389113+rebkova@users.noreply.github.com>
2020-06-02 18:53:49 +0200

    Add files via upload

---
+++ Selection_sort.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Selection_sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] testArray = { 0, -5, -99, 4, 9, 7, 6 };
+            SelectionSort(testArray);
+            Print_Array(testArray);
+
+        }
+
+        private static void SelectionSort(int[] array)
+        {
+            /*selection sorting in an array*/
+            for (int currentIndex = 0; currentIndex < array.Length; currentIndex++)
+            {
+                int currentMinIndex = IndexOfMinimum(array, currentIndex);
+                if (array[currentMinIndex] < array[currentIndex])
+                {
+                    Swap(array, currentMinIndex, currentIndex);
+                }
+            }
+        }
+
+        private static int IndexOfMinimum (int[] array, int startIndex)
+        {
+            /*method for finding min value of the array*/
+            int minValue = array[startIndex];
+            int minIndex = startIndex;
+
+            for (var i = minIndex + 1; i < array.Length; i++)
+            {
+                if (array[i] < minValue)
+                {
+                    minIndex = i;
+                    minValue = array[i];
+                }
+            }
+            return minIndex;
+
+        }
+
+        private static void Swap(int[] array, int firstIndex, int secondIndex)
+        {
+            /*method for swapping items in an array*/
+            int temp = array[firstIndex];
+            array[firstIndex] = array[secondIndex];
+            array[secondIndex] = temp;
+        }
+
+        private static void Print_Array(int[] array)
+        {
+            /*function for printing array*/
+            Console.Write("Test array after sorting: { ");
+            foreach (int item in array)
+            {
+                Console.Write(item + ", ");
+            }
+            Console.Write("}");
+        }
+    }
+}
